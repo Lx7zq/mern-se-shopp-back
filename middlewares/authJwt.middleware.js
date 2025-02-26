c5595eaf0578fee7a0b41b9d9fad5aeded24252e
Lx7zq <thisforworkonly7@gmail.com>
2025-02-26 16:51:20 +0700

    finish

--- middlewares/authJwt.middleware.js
+++ middlewares/authJwt.middleware.js
@@ -9,14 +9,22 @@ verifyToken = (req, res, next) => {
   }
   jwt.verify(token, secret, (err, decoded) => {
     if (err) return res.status(403).json({ message: "Access Forbidden!" });
-    req.userId = decoded.id;
-    req.username = decoded.username;
+    req.role = decoded.role;
+    req.email = decoded.email;
     next();
   });
 };
 
+isAdmin = async (req, res, next) => {
+  if (req.role !== "admin") {
+    return res.status(403).json({ message: "require Admin role!" });
+  }
+  next();
+};
+
 const authJwt = {
   verifyToken,
+  isAdmin,
 };
 
 module.exports = authJwt;
