995241ea89a545d1a2cb5d0220f89e6b5c970e23
Lx7zq <thisforworkonly7@gmail.com>
2025-02-27 12:09:16 +0700

    finish

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const userRouter = require("./routers/user.router");
 const productRouter = require("./routers/product.router");
 const cartRouter = require("./routers/cart.router");
+const stripeRouter = require("./routers/stripe.router");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./docs/swagger-output.json");
 const app = express();
@@ -33,6 +34,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/product", productRouter);
 app.use("/api/v1/carts", cartRouter);
+app.use("/api/v1/stripe", stripeRouter);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
